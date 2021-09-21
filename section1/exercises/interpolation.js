40b80da466bdbced8545d6194bde00249b98f9c4
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2021-09-20 21:21:14 -0600

    Add Section 1 Work

--- section1/exercises/interpolation.js
+++ section1/exercises/interpolation.js
@@ -22,8 +22,10 @@ var creatures = "unicorns";
 console.log( `My zoo has ${numberOfCreatures} ${creatures}!`);
 
 // YOU DO: What data type will be logged to the console as a result of line 22? Explain.
-// Your answer and explanation here: 
+// Your answer and explanation here:
 
+// My zoo has 7 unicorns!
+// the log pulls the information from the var and plugs these values in to the string.
 
 // YOU DO:
 // Write code that combines the variables below into a string that
@@ -31,6 +33,7 @@ console.log( `My zoo has ${numberOfCreatures} ${creatures}!`);
 var speedy = "quick red fox";
 var slowPoke = "lazy brown dog";
 
+console.log(`The ${speedy} jumps over the ${slowPoke}`);
 
 // YOU DO:
 // Write code that combines the variables below into a string that
@@ -40,16 +43,23 @@ var slowPoke = "lazy brown dog";
 slowPoke = "tortoise";
 speedy = "hare";
 
+console.log(`In a predicable result, the ${slowPoke} beat the ${speedy}`);
+
 
 // YOU DO:
-// Declare three variables, name/content/data type of your choice. Think carefully about what 
+// Declare three variables, name/content/data type of your choice. Think carefully about what
   // you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!)
-  // Then, log out ONE sentence that incorporates all THREE variables uisng interpolation. 
+  // Then, log out ONE sentence that incorporates all THREE variables uisng interpolation.
 
+var catType = "lazy";
+var catSize = "chonk";
+var object = "ham"
+
+console.log(`The ${catType} ${catSize} cat is also a fine ${object}.`);
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
+// Did you run this file in your terminal to make sure everything printed out to the console
   // as you would expect? You should be doing this by now, and getting into the habit of it.
