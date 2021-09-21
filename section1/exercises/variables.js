6b77bbd709217a27e735d384dda54d50d68e7304
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2021-09-20 21:33:51 -0600

    Add exercise variables.

--- section1/exercises/variables.js
+++ section1/exercises/variables.js
@@ -1,3 +1,4 @@
+// variables.js
 /*
 In the exercises below, write your own code where indicated
 to achieve the desired result.
@@ -13,32 +14,52 @@ file from your terminal with the command `node section1/exercises/variables.js`
 // EXAMPLE: Write code below to assign Harry Potter's name to a variable and
 // log that variable to the console:
 var name = "Harry Potter";
-console.log(name);
+var team = "The Holyhead Harpies";
+
+console.log(`${name}'s favorite Quidditch team is ${team}`);
+
 
 // EXAMPLE: Write code below to add 2 to the variable `students` and
 // log the result:
 var enrolledStudents = 22 + 2;
+
 console.log(enrolledStudents);
 
 // YOU DO:
 // Write code below to save the string 'Harry Potter must not return to Hogwarts!'
 // log that variable to the console.
+var dobbySays = 'Harry Potter must not return to Hogwarts!';
 
+console.log(`${dobbySays}`)
 
 // YOU DO:
-// Declare three variables, named `firstName`, `isHungry` and `numberOfPets`. 
+// Declare three variables, named `firstName`, `isHungry` and `numberOfPets`.
   // Store the appropriate data types in each.
 // log all three variables to the console.
+var firstName = "Nagini";
+var isHungry = "Yes";
+var numberOfPets = "200";
+
+console.log(`Is ${firstName} hungry? ${isHungry}. Oh no! We have ${numberOfPets} ${firstName}'s to feed!`)
 
 // IN WORDS:
-// How did you decide to use the data type you did for each of the three variables above? 
+// How did you decide to use the data type you did for each of the three variables above?
   // Explain.
 
 
+//  First I thought of  a sentence that I believed would make sense.
+//  Then I assigned the variable types based on what I wanted to say.
+
 // YOU DO:
-// Re-assign the values to the three variables from the previous challenge to different 
+// Re-assign the values to the three variables from the previous challenge to different
   // values (but same data type).
 // log all three variables to the console.
+var firstName = "Bethany the Lion";
+var isHungry = "No... well, maybe a little.";
+var numberOfPets = "53042";
+
+console.log(`Is ${firstName} hungry? ${isHungry}.
+  God help us all! We have ${numberOfPets} ${firstName}'s to feed!`)
 
 
 // YOU DO:
@@ -46,10 +67,19 @@ console.log(enrolledStudents);
 var healthySnacks = 6;
 var junkFoodSnacks = 8;
 
-
+console.log(`I have ${healthySnacks} carrots and ${junkFoodSnacks} bags of chips!`)
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
-// as you would expect? 
+// Did you run this file in your terminal to make sure everything printed out to the console
+// as you would expect?
+
+// node variables.js
+//Harry Potter's favorite Quidditch team is The Holyhead Harpies
+//24
+//Harry Potter must not return to Hogwarts!
+//Is Nagini hungry? Yes. Oh no! We have 200 Nagini's to feed!
+//Is Jimmy the Lion hungry? No... well, maybe a little..
+//  God help us all! We have 53042 Jimmy the Lion's to feed!
+// I have 6 carrots and 8 bags of chips!
